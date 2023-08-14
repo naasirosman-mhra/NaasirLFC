9df76ba7c6a9e4cc0f687f5617d265792fb7f00c
Osman <Naasir.Osman@mhra.gov.uk>
2023-08-14 09:42:58 +0100

    testing CI workflow

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-Bye World
+goodBye World
         </p>
         <a
           className="App-link"
