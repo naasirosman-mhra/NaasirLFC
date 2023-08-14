17ad2fcad2e929f357b6c8313ede0ced781b121e
Osman <Naasir.Osman@mhra.gov.uk>
2023-08-14 12:28:52 +0100

    testing if IaC workflow gets  triggered (it shouldnt)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-goodBye World
+goodBye World for now lol
         </p>
         <a
           className="App-link"
